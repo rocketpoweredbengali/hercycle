5c2ed564c90064775171ef33b6e31a6044b8215c
Sourjya Biswas <sourjyabiswas03@gmail.com>
2025-08-24 18:16:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn("font-body", fontBody.variable, fontHeadline.variable)}>
         {children}
         <Toaster />
